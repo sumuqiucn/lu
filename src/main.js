902ba46522e631d2a9ad0b18c7dcaec49572ef1d
sumuqiu <1515498325@qq.com>
2022-02-08 19:16:42 +0800

    2022End0208DAY1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
+// element-ui的完整引入
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
@@ -14,12 +15,20 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-import HintButton from'@/components/HintButton'
+import '@/plugins/vcharts'
 
+import *  as API  from '@/api/product'
 
+import CategorySelector from '@/components/CategorySelector'
+import HintButton from '@/components/HintButton'
 
-
+Vue.component('CategorySelector',CategorySelector)
 Vue.component('HintButton',HintButton)
+
+// {
+//   trademark,
+//   attr
+// }
 
 
 /**
@@ -41,6 +50,8 @@ Vue.use(ElementUI, { locale })
 // Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+Vue.prototype.$API = API
+
 
 new Vue({
   el: '#app',
